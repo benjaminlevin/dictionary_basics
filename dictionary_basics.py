768a9c78f1daf50f1e408e0db5e703948a1d4ff5
Benjamin Levin <benjaminwlevin@gmail.com>
2017-06-09 19:59:34 -0400

    first commit

---
+++ dictionary_basics.py
@@ -0,0 +1,15 @@
+test_dictionary = {
+    "name_first": "Ben",
+    "name_last": "Levin",
+    "age": 27,
+    "country": "The United States",
+    "current_language":
+    "Python"
+    }
+    
+def test_fun():
+    print "My name is", test_dictionary["name_first"]
+    print "My age is", test_dictionary["age"]
+    print "My country of origin is", test_dictionary["country"]
+    print "The language I am currently learning is", test_dictionary["current_language"]
+test_fun()
